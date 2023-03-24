230516aca69a832e0cf0f02ca2e30ad830cec800
Danne Consult <danneconsult@gmail.com>
2023-03-24 10:07:22 +0300

    controller get samples null value error removed

--- controller/getsamples.php
+++ controller/getsamples.php
@@ -1,10 +1,13 @@
 <?php
-    include "../manage/_db/dbconf.php";
-    $db = new DBconnect;
-    $prefix = $db->prefix;
-    $recid = $_GET['recid'];
-    $swords = "SELECT * FROM ".$prefix."coverletter_formats WHERE id='$recid '";
-    $resultx = $db->conn->query($swords);
-    $rwlist = $resultx->fetch_array();
-    echo $rwlist['samplewords'];
+    if(isset($_GET['recid']) && $_GET['recid']!==""){
+        include "../manage/_db/dbconf.php";
+        $db = new DBconnect;
+        $prefix = $db->prefix;
+        $recid = $_GET['recid'];
+        $swords = "SELECT * FROM ".$prefix."coverletter_formats WHERE id='$recid '";
+        $resultx = $db->conn->query($swords);
+        $rwlist = $resultx->fetch_array();
+        echo $rwlist['samplewords'];
+    }
+
 ?>
