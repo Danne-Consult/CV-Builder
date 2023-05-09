ddac8df710ca12343512ad88a9529e2a4890ded3
Danne Consult <danneconsult@gmail.com>
2023-05-09 09:03:00 +0300

    pagination styles added

--- manage/payments.php
+++ manage/payments.php
@@ -1,5 +1,8 @@
 <?php 
     session_start();
+
+    include_once "_db/dbconf.php";
+
     if(!isset($_SESSION['admin'])){
         header('location:login.php');
     }
@@ -23,19 +26,53 @@
 </head>
 <body>
     <?php include "includes/nav/header.inc"; ?>
-    <div class="container12">
+    <div class="container12 workbx">
         <article>
-            <h3>Payments</h3>
+            
             <div class="row">
-                <div class="col-lg-4">
+                <div class="col-lg-1">
                     <?php include "includes/nav/sidenav.inc"; ?>
                 </div>
-                <div class="col-lg-8">
-                    
+                <div class="col-lg-11">
+                    <h3>Payments</h3>
+                    <table class="sorttablesearch">
+                    <thead>
+                        <tr>
+                            <th>Payment Ref</th>
+                            <th>User's Name</th>
+                            <th>Paid on</th>
+                            <th>Sub Type</th>
+                            <th>View</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        
+                        <?php
+                            $db = new DBconnect;
+                            $prefix = $db->prefix;
+                            $sql="SELECT * FROM ".$prefix."invoices a LEFT JOIN ".$prefix."users b ON a.userid = b.usercode WHERE a.paystatus='paid' LIMIT 10";
+                            $result= $db->conn->query($sql);
+                            $tempbx="";
+                            $temptype="";
+                            while($rws = $result->fetch_array()){
+                                $m = "<tr><td>".$rws['invoiceno']."</td>";
+                                $m .= "<td>".$rws['fname']."&nbsp;".$rws['lname']."</td>";
+                                $m .= "<td> ".$rws['paidon']."</td>";
+                                $m .= "<td> ".$rws['paytype']."</td>";
+                                $m .= "<td><a style='text-align:center; display:block' href='viewuser.php?u=".$rws['usercode']."'><i class='fa-solid fa-eye'></i></a></td>";
+                                $m .= "</tr>";
+
+                                echo $m;
+                            }
+                        ?>
+                        
+                    </tbody>
+</table>
                 </div>
             </div>
         </article>
     </div>
     <?php include "includes/nav/footer.inc"; ?>
+    <?php include "includes/scripts.inc"; ?>
 </body>
 </html>
